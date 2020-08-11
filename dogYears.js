9c8aa3913c2abf31fb4fb888941ba963d6d71f01
mohammed-mohsin <mohammedmohsin0725@gmail.com>
2020-08-11 13:23:39 +0600

    Dog Years

---
+++ dogYears.js
@@ -0,0 +1,22 @@
+// My Age
+const myAge = 22;
+
+// Early Years
+let earlyYears = 2
+
+// Dog Years increase by 10.5
+earlyYears *= 10.5;
+
+// Decrease 2 years
+let laterYears = myAge - 2;
+
+// After 4 years My age
+laterYears = myAge * 4;
+
+// My Dog's Currebt Ages
+let myAgeInDogYears = earlyYears + laterYears;
+
+//  My Name
+const myName = `Mohammed Mohsin`.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
